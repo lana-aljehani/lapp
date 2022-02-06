99752b8161385da37deea99e46d156da9df61b61
lana-aljehani <98253339+lana-aljehani@users.noreply.github.com>
2022-02-06 10:54:58 +0300

    lana

--- lapp.251/src/lapp/pkg251/Lapp251.java
+++ lapp.251/src/lapp/pkg251/Lapp251.java
@@ -17,7 +17,8 @@ public class Lapp251 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-        //test 
+        //test \
+        //test
     }
     
 }
